234bf1c6872bf0d00435bcc66ea214471ef44d59
Tomek586 <tomasz.dziedzic@microsoft.wsei.edu.pl>
2023-03-06 13:49:16 +0100

    GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,16 +1,52 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count <5)
+            {
+                throw new InvalidOperationException("InvalidOperationException");
+            }
+
+
+            var top20percent = Students.Count / 5;
+
+           
+            List<double> Grades = new List<double>();
+            foreach (var student in Students.OrderByDescending(student => student.AverageGrade))
+            {
+                Grades.Add(student.AverageGrade);
+            }
+
+            if (averageGrade > (Grades[top20percent]))
+                return 'A';
+            else if (averageGrade > (Grades[top20percent * 2]))
+                return 'B';
+            else if (averageGrade > (Grades[top20percent * 3]))
+                return 'C';
+            else if (averageGrade > (Grades[top20percent * 4]))
+                return 'D';
+            else
+                return 'F';
+
+
+
+
+        }
+
+
     }
 }
 
